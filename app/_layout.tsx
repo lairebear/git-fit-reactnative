184c6810354759e306234a1f88fddd39682fa239
laire <rncafirma@gmail.com>
2025-10-04 15:00:35 -0400

    tried to debug stuff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,11 @@
-import { Tabs } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Tabs
+    <Stack
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "#0284c7",
-        tabBarInactiveTintColor: "#94a3b8",
       }}
-    >
-      <Tabs.Screen
-        name="dashboard"
-        options={{
-          title: "Dashboard",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="workoutlog"
-        options={{
-          title: "Workout Log",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="barbell" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="exercisecatalog"
-        options={{
-          title: "Exercises",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="list" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tabs>
+    />
   );
 }
